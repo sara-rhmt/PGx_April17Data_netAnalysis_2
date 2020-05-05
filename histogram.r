22a80af70d77254703d9db464b9e83917de86ab2
Sara <srahmati@VCMM.local>
2020-05-04 23:11:17 -0400

    debug histogram.r

--- histogram.r
+++ histogram.r
@@ -1,6 +1,6 @@
 my.words <- read.table("words.txt", header = F, as.is = T, stringsAsFactors = F, quote = "\"", sep = "\t")
 
-Length = nchar(my.words[, 1]
+Length = nchar(my.words[, 1])
 words.freq <- as.data.frame(table(nchar(my.words[, 1])), stringsAsFactors = F)
 
 write.table(words.freq, file = "histogram.tsv", quote = F, row.names = F, sep = "\t")
